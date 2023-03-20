aa6cf1e59f6fb82c8ca43feb7de39375d2c98619
AleksTimofeev <timoha144a@gmail.com>
2023-03-21 00:25:37 +0300

    add types noteReducer

--- src/store/noteReducer.ts
+++ src/store/noteReducer.ts
@@ -4,10 +4,39 @@ import { v4 as uuid4 } from 'uuid'
 const slice = createSlice({
   name: 'noteReducer',
   initialState: {
-    noteData: [],
-    tagsList: []
-  },
+    noteData: {
+      '001': [
+        {id: '001001', text: 'text 001001', tags: ['001', '002']},
+        {id: '001002', text: 'text 001002', tags: ['002']},
+        {id: '001003', text: 'text 001003', tags: ['001']}
+      ],
+      '002': [
+        {id: '002001', text: 'text 002001', tags: ['001', '002']},
+        {id: '002002', text: 'text 002002', tags: ['001']},
+        {id: '002003', text: 'text 002003', tags: ['003']}
+      ],
+      '003': [
+        {id: '003001', text: 'text 003001', tags: ['003']},
+        {id: '003002', text: 'text 003002', tags: ['002']},
+        {id: '003003', text: 'text 003003', tags: ['001', '002', '003']}
+      ]
+    },
+    tagsList: ['001', '002', '003']
+  }as InitialStateType,
   reducers: {}
 })
 
 export const noteReducer = slice.reducer
+
+
+type InitialStateType = {
+  noteData: {
+    [key:string]: NoteType[]
+  },
+  tagsList: string[]
+}
+type NoteType = {
+  id: string,
+  text: string,
+  tags: string[]
+}
