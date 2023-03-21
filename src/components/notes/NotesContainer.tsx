f47291a870956986cc1203fb93cd0d303de8a2ff
AleksTimofeev <timoha144a@gmail.com>
2023-03-21 13:54:43 +0300

    refactor render all notes

--- src/components/notes/NotesContainer.tsx
+++ src/components/notes/NotesContainer.tsx
@@ -4,6 +4,7 @@ import {useAppSelector} from "../../store/store";
 import {Note} from "./note/Note";
 import styles from './NotesContainer.module.scss'
 import {AddNote} from "./addNote/AddNote";
+import {NoteType} from "../../store/noteReducer";
 
 export const NotesContainer = () => {
 
@@ -11,17 +12,12 @@ export const NotesContainer = () => {
   const {tag} = params
   const tags = useAppSelector(state => state.notes.tagsList)
   const notes = useAppSelector(state => state.notes.noteData)
-  const notesForRender = notes.filter(item => item.tags.some(value => value === tag))
-  // if(tag === undefined){
-  //   tags.map(item => {
-  //     notesForRender.push(...notes[item])
-  //   })
-  // }else if(tag && notes[tag]){
-  //   notesForRender.push(...notes[tag])
-  // }
-
-
-
+  let notesForRender: NoteType[] = []
+  if (tag === undefined || !tags.some(value => value === tag)){
+    notesForRender = notes
+  }else{
+    notesForRender = notes.filter(item => item.tags.some(value => value === tag))
+  }
 
   return (
     <div className={styles.wrapper}>
